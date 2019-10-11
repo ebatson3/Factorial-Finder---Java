f81407a47e850e4206ebbb3873f47e094c198d55
Evelyn Batson <55146641+ebatson3@users.noreply.github.com>
2019-10-11 15:42:54 -0400

    Add files via upload
    This is an alternative program to find the factorial of an integer. This one uses functions as opposed to just simple loops

--- Main.java
+++ Main.java
@@ -1,40 +1,43 @@
-package ebatson3_lab04_q1; //Package name
+package ebatson3_lab05_q1; //Package name
 
-import java.util.Scanner; //Importing scanner for user input
+import java.util.Scanner; //Importing the scanner for user input
 
 public class Main {
-	public static void main(String[] xyz) {
-		int n = 0; //Declaring initial variable value
-		double nfact = 1; //Declaring initial variable value
-		boolean check = true;
-		
-	System.out.println("*******************************************************\r\n" +
-		 "ES1036b: Lab 04 Q1\r\n" + "Date: 2019-02-26 \r\n" +
-		 "Name: Evelyn Batson \r\n" + "Stuent Number: 251 010 073 \r\n" +
-		 "********************************************** \r\n"
-		 + "This program will determine the factorial of an integer");
-	/*Outputs purpose of program to user  */
 	
-	Scanner inputScanner = new Scanner(System.in); //Naming and initializing scanner
-	System.out.println("Please enter an integer: "); //Prompting user input
-	n = inputScanner.nextInt(); //This value assigned to n variable
-	while (check) 
-	{
-		if (n<0) {
-		System.out.println("Invalid input, please enter a postitive integer: "); 
-		/*Displayed if input does not meet requirements */
-		n = inputScanner.nextInt();
+	public static void printHeader (int labNu, int questionNum, String fName, String lName, String mission) {
+		
+	}
+	
+	public static int getPositiveInteger() {
+		boolean checker = true; //Initializing boolean to check if valid input
+		int value = 0; //Naming input variable
+		Scanner inputScanner = new Scanner(System.in); //Naming scanner
+		System.out.println("Please enter the number to be turned into it's factorial: "); //Outputs this to get user input
+		value = inputScanner.nextInt(); //Assigns this value to the variable
+		if (value<0) {
+			System.out.println("Invalid! Enter a positive number dood: "); //Outputs when input is invalid
+			value = inputScanner.nextInt(); //Assigns this value to the variable
 		}
 		else
-			check = false;
+			checker = false; //Changes the value of the boolean
+		
+		inputScanner.close(); //Closing the scanner
+		return value; //Returns this vale to the main method
 	}
 	
-	while (n > 0) { //Opening while loop
-		nfact = nfact*n; //Computing the factorial value
-		n--; //Decrementing the input
+	public static double computeFactorial(int value) {
+		int yfact = 1; // Declaring the factorial variable 
+		while (value>0) {
+			yfact = yfact*value; //Computing the factorial using a loop 
+			value--; //Decrementing the input value
+		}
+		return yfact; //Returns this value to the main method
 	}
-	System.out.println("The factorial is " + nfact); //Outputs the result to the user
-	inputScanner.close(); //Closing the scanner
-	System.out.println("Goodbye!"); //Outputs goodbye to user
+	
+	public static void main(String[] xyz) {
+		System.out.println("The factorial is: " + computeFactorial(getPositiveInteger())); //Outputs the result using the methods above
 	}
 } //End of program
+		
+	
+
