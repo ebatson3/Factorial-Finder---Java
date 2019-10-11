58bbc265efa56e76785a718af496b0a8d3e82c45
ebatson3 <55146641+ebatson3@users.noreply.github.com>
2019-10-11 15:12:43 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+package ebatson3_lab04_q1; //Package name
+
+import java.util.Scanner; //Importing scanner for user input
+
+public class Main {
+	public static void main(String[] xyz) {
+		int n = 0; //Declaring initial variable value
+		double nfact = 1; //Declaring initial variable value
+		boolean check = true;
+		
+	System.out.println("*******************************************************\r\n" +
+		 "ES1036b: Lab 04 Q1\r\n" + "Date: 2019-02-26 \r\n" +
+		 "Name: Evelyn Batson \r\n" + "Stuent Number: 251 010 073 \r\n" +
+		 "********************************************** \r\n"
+		 + "This program will determine the factorial of an integer");
+	/*Outputs purpose of program to user  */
+	
+	Scanner inputScanner = new Scanner(System.in); //Naming and initializing scanner
+	System.out.println("Please enter an integer: "); //Prompting user input
+	n = inputScanner.nextInt(); //This value assigned to n variable
+	while (check) 
+	{
+		if (n<0) {
+		System.out.println("Invalid input, please enter a postitive integer: "); 
+		/*Displayed if input does not meet requirements */
+		n = inputScanner.nextInt();
+		}
+		else
+			check = false;
+	}
+	
+	while (n > 0) { //Opening while loop
+		nfact = nfact*n; //Computing the factorial value
+		n--; //Decrementing the input
+	}
+	System.out.println("The factorial is " + nfact); //Outputs the result to the user
+	inputScanner.close(); //Closing the scanner
+	System.out.println("Goodbye!"); //Outputs goodbye to user
+	}
+} //End of program
